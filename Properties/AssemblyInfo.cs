eb13950bb6c3c26d2945712ceb564452e3885930
Rick Parrish <github@rick.parrish>
2013-07-20 17:35:09 -0400

    Add copyright header

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,23 @@
-﻿using System.Reflection;
+﻿/*
+  TelnetDoor: A BBS door to allow outgoing telnet or rlogin connections
+  Copyright (C) 2013  Rick Parrish, R&M Software
+
+  This file is part of TelnetDoor.
+
+  TelnetDoor is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  TelnetDoor is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with TelnetDoor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
